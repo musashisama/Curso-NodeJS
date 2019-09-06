5db63a04b1ba1e615107f481e2123bc340159704
musashisama <48837659+musashisama@users.noreply.github.com>
2019-09-06 16:07:16 -0300

    Início da Parte 2. CSS aplicado.

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -1,56 +1,48 @@
-class LivroDAO {
+class LivroDao {
 
     constructor(db) {
         this._db = db;
     }
 
     adiciona(livro) {
-
         return new Promise((resolve, reject) => {
             this._db.run(`
-            INSERT INTO LIVROS (
-                    titulo,
+                INSERT INTO livros (
+                    titulo, 
                     preco,
                     descricao
-                ) values (?, ?, ?)
-            `,[
-                livro.titulo, 
-                livro.preco, 
-                livro.descricao
-            ], function(err) {
-                if(err) {
-                    console.log(err);
-                    return reject("Não foi possível adicionar o livro.");
+                ) values (?,?,?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+
+                    resolve();
                 }
-
-                resolve();
-
-            });
+            )
         });
-
     }
 
     lista() {
-        return new Promise((resolve,reject)=> {
+        return new Promise((resolve, reject) => {
             this._db.all(
                 'SELECT * FROM livros',
-                (erro,resultados) => {
-                if(erro) return reject('Não foi possível listar os livros.!');
-                return resolve(resultados);
-            }
-            
-            )
+                (erro, resultados) => {
+                    if (erro) return reject('Não foi possível listar os livros!');
 
-        })
-        
+                    return resolve(resultados);
+                }
+            )
+        });
     }
-    /* Com callback:
-        lista(callback) {
-        this._db.all(
-            'SELECT * FROM livros',
-            (erro,resultados) => callback(erro, resultados)
-        )
-    } */
+
     buscaPorId(id) {
 
         return new Promise((resolve, reject) => {
@@ -99,7 +91,7 @@ class LivroDAO {
     remove(id) {
 
         return new Promise((resolve, reject) => {
-            this._db.run(
+            this._db.get(
                 `
                     DELETE 
                     FROM livros
@@ -115,7 +107,6 @@ class LivroDAO {
             );
         });
     }
-    
 }
 
-module.exports = LivroDAO;
+module.exports = LivroDao;
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -2,98 +2,67 @@ const LivroDao = require('../infra/livro-dao');
 const db = require('../../config/database');
 
 module.exports = (app) => {
-
-    app.get('/', function(req,resp) {
-        resp.send( `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>            
-            </body> 
-        </html>
-        `);
+    app.get('/', function(req, resp) {
+        resp.marko(
+            require('../views/base/home/home.marko')
+        );
     });
     
-    app.get('/livros', function(req,resp) {
+    app.get('/livros', function(req, resp) {
 
         const livroDao = new LivroDao(db);
-
-        //Com Promises:
         livroDao.lista()
-        .then(resultados => resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: resultados
-            }
-        ))
-        .catch(erro => console.log(erro));
-
-        /* Com callback:
-            livroDao.lista(function(erro,resultados) {
-            resp.marko(
-                require('../views/livros/lista/lista.marko'),
-                {
-                    livros: resultados
-                }
-            ); 
-
-        });*/
-
-
-        /* db.all('SELECT * FROM livros', function(erro, resultados) {
-            resp.marko(
-                require('../views/livros/lista/lista.marko'),
-                {
-                    livros: resultados
-                }
-            );
-
-        }) */
-        
+                .then(livros => resp.marko(
+                    require('../views/livros/lista/lista.marko'),
+                    {
+                        livros: livros
+                    }
+                ))
+                .catch(erro => console.log(erro));
     });
 
-    app.get('/livros/form', function(req, resp){
+    app.get('/livros/form', function(req, resp) {
         resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
-    });
-
-    app.post('/livros',function(req, resp){
-        const livroDao = new LivroDao(db);
-        console.log(req.body);
-        livroDao.adiciona(req.body)
-        .then(resp.redirect('/livros'))
-        .catch(erro => console.log(erro));
-    });
-
-    app.put('/livros',function(req, resp){
-        const livroDao = new LivroDao(db);
-        console.log(req.body);
-        livroDao.atualiza(req.body)
-        .then(resp.redirect('/livros'))
-        .catch(erro => console.log(erro));
-    });
-
-    app.delete('/livros/:id', function(req, resp){
-        const id = req.params.id;
-        const livroDao = new LivroDao(db);
-        livroDao.remove(id)
-        .then(() => {resp.status(200).end()})
-        .catch(erro => console.log(erro));
     });
 
     app.get('/livros/form/:id', function(req, resp) {
         const id = req.params.id;
         const livroDao = new LivroDao(db);
-    
+
         livroDao.buscaPorId(id)
-            .then(livro => 
-                resp.marko(
-                    require('../views/livros/form/form.marko'),
-                    { livro: livro }
+                .then(livro => 
+                    resp.marko(
+                        require('../views/livros/form/form.marko'), 
+                        { livro: livro }
+                    )
                 )
-            )
-            .catch(erro => console.log(erro));
-    
+                .catch(erro => console.log(erro));
+    });
+
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.atualiza(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.delete('/livros/:id', function(req, resp) {
+        const id = req.params.id;
+
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+                .then(() => resp.status(200).end())
+                .catch(erro => console.log(erro));
     });
 };
